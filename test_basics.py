31cbe2eab13f339fbd2730e2528166ed4236d60a
mick-warehime <mickwarehime@gmail.com>
2017-11-21 21:46:09 -0800

    test tutorial skill cycling through each output with saved state in between

--- test_basics.py
+++ test_basics.py
@@ -3,16 +3,27 @@ from lambda_function import *
 import json
 
 
-class Test_Basics(TestCase):
+def response_text(response):
+    return response['response']['outputSpeech']['text']
+
+
+class TestBasics(TestCase):
     def setUp(self):
         self.scene_name = 'tutorial'
         self.json = json.load(open('scenes/tutorial.json'))
+        self.fire_intent = {'name': 'fireIntent'}
 
-    def test_one_round(self):
-        response = loadScene(self.scene_name, {}, {})
-        first_round = response['response']['outputSpeech']['text']
+    def test_fire_five_times(self):
+        session_attributes = {}
 
-        expected_first_round = self.json['rounds'][0]
-        self.assertEqual(first_round, expected_first_round)
+        for i in range(5):
+            # the tutorial skill requires the user to say 'fire' 5 times
+            response = loadScene(self.scene_name, self.fire_intent, session_attributes)
 
+            # after each 'fire' make sure we read the correct statement from the tutorial json
+            text = response_text(response)
+            expected_text = self.json['rounds'][i]
+            self.assertEqual(text, expected_text)
 
+            # this maintains our state throughout the game - one variable 'round' = 0-5
+            session_attributes = response['sessionAttributes']
