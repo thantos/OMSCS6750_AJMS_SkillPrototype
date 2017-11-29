ce9f8ffc22b1670c83a123cf830a70ba6f0d9d21
mick-warehime <mickwarehime@gmail.com>
2017-11-28 22:01:25 -0800

    none-safe the update

--- boxing/match.py
+++ boxing/match.py
@@ -79,7 +79,7 @@ def resolve_turn(game_state):
 
 def update_with_intent(intent_data, session):
     if not session:
-        initialize({})
+        session = initialize({})
     player_move = get_move_from_intent(intent_data)
     session[PLAYERMOVE] = player_move
     session = update(session)
